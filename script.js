8cd5f151090954eec9e866984814a9e0ed0b5501
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-20 20:25:34 -0700

    working on going through a series of prompt questions to get some user input

--- script.js
+++ script.js
@@ -22,32 +22,55 @@ function generatePassword( ){
   
   
   if(questionOne !== "yes" && questionOne !== "Yes"){
-  
-   var new_word =[];
-   var newer_word = [];
-    for(var i = 0; i < questionOne.length;i++){
-      
-       new_word.push(questionOne.charCodeAt(i));
-       newer_word.push(String.fromCharCode(new_word[i] + 2))
-      
-       
-    }
-  var char_location = newer_word.join('');
-    return char_location;
+    
+    return "No new password needed!";
   
   } else {
   
   
-    return "Okay new password, Got it!?";  
-   
- 
+    var questionTwo = function(a){ 
+      questionTwoValue = prompt(a);
+     }
+    var questionTwoValue = 0;  
+  
+       questionTwo("Okay then, How long is this password?");
+
+    while(questionTwoValue < 7 || questionTwoValue > 128){
+
+
+      questionTwo("Error. Please choose betweem 8 and 128 characters long");
+      
+    }
+
+    var questionThree = function(b){
+      questionThreeValue = prompt(b)
+    }
+    var questionThreeValue = '';
+    questionThree("Do you need special characters?");
+
+    while(questionThreeValue !== "yes" && questionThreeValue !== "Yes" && questionThreeValue !== "no" && questionThreeValue !== "No"){
+      questionThree("Please be serious, Do you need special characters?");
+    }
+    if(questionThreeValue === "No" || questionThreeValue === "no"){
+      console.log(questionTwoValue)
+    } else {
+      console.log(questionTwoValue + " and " + questionThreeValue);
+    }
   }
-
-
 }
 
 
 
 
 
-
+// var new_word =[];
+// var newer_word = [];
+//  for(var i = 0; i < questionOne.length;i++){
+   
+//     new_word.push(questionOne.charCodeAt(i));
+//     newer_word.push(String.fromCharCode(new_word[i] + 2))
+   
+    
+//  }
+// var char_location = newer_word.join('');
+//  return char_location;
