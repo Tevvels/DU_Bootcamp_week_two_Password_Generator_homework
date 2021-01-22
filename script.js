a2a57b36dc100811dbd48a561ecb300b1a62a26b
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-22 09:42:45 -0700

    changed the function of the homework to try out the fisher yates shuffle

--- script.js
+++ script.js
@@ -15,80 +15,142 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+
+
+
+
+
+// recreating this project using the Fisher-Yates Shuffle
+
+function mixArray(array){
+  var index = 12;
+  var tempValue;
+  var newIndex;
+
+  while (index !== 0){
+    newIndex = Math.floor(Math.random() * index);
+    index -= 1;
+
+
+    tempValue = array[index];
+    array[index] = array[newIndex];
+    array[newIndex] = tempValue;
+
+  }
+
+  return array;
+
+}
+
+
+var testArray = ['a','b','c','A','B','C',1,2,3,"!","@","#"];
+mixArray(testArray);
+
+console.log(testArray);
+
+
+//works well. :D 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //// customs////
 
-function generatePassword( ){
+// function generatePassword( ){
 
 
-  var questionOne = prompt("need a new password?");
+//   var questionOne = prompt("need a new password?");
   
   
-  if(questionOne !== "yes" && questionOne !== "Yes"){
+//   if(questionOne !== "yes" && questionOne !== "Yes"){
     
-    return "No new password needed!";
+//     return "No new password needed!";
   
-  } else {
+//   } else {
   
   
-    var questionTwo = function(a){ 
-      questionTwoValue = prompt(a);
-     }
-    var questionTwoValue = 0;  
+//     var questionTwo = function(a){ 
+//       questionTwoValue = prompt(a);
+//      }
+//     var questionTwoValue = 0;  
   
-       questionTwo("Okay then, How long is this password?");
+//        questionTwo("Okay then, How long is this password?");
 
-    while(questionTwoValue < 7 || questionTwoValue > 128 || questionTwoValue === NaN){
+//     while(questionTwoValue < 7 || questionTwoValue > 128 || questionTwoValue === NaN){
 
 
-      questionTwo("Error. Please choose betweem 8 and 128 characters long");
+//       questionTwo("Error. Please choose betweem 8 and 128 characters long");
       
-    }
+//     }
 
-    var questionThree = function(b){
-      questionThreeValue = prompt(b)
-    }
-    var questionThreeValue = '';
-    questionThree("Do you need special characters?");
-
-    while(questionThreeValue !== "yes" && questionThreeValue !== "Yes" && questionThreeValue !== "no" && questionThreeValue !== "No"){
-      questionThree("Please be serious, Do you need special characters?");
-    }
-    return wordGenerator(parseInt(questionTwoValue),questionThreeValue);
-
-  }
-}
-
-wordGenerator = function(a,b){
-// var new_word =[];
-var newer_word = [];
-var randomChar = Math.round(Math.random() *128);
-if(b === "yes" || b === "Yes"){
-
-  for(var i = 0; i < a;i++){
-    randomChar = Math.round(Math.random() *128);
-    while(randomChar < 31 || randomChar > 127){
-      randomChar = Math.round(Math.random() *128);
-    }
-      // new_word.push(questionOne.charCodeAt(i));
-      newer_word.push(String.fromCharCode(randomChar));
-    
-      
-  }
-} else {
-
-  for(var i = 0; i < a;i++){
-    randomChar = Math.round(Math.random() *128);
-    while((randomChar < 65 || randomChar > 90)&&(randomChar < 97 || randomChar > 122)){
-      randomChar = Math.round(Math.random() *128);
-    }
-      // new_word.push(questionOne.charCodeAt(i));
-      newer_word.push(String.fromCharCode(randomChar));
+//     var questionThree = function(b){
+//       questionThreeValue = prompt(b)
      
-  }
-}
-var char_location = newer_word.join('');
-console.log(newer_word)
-return char_location;
+//     }
+//     var questionThreeValue = '';
+//     questionThree("Do you need special characters?");
+//      questionThreeValue = questionThreeValue.toUpperCase();
+//     console.log(questionThreeValue);
+//     if( questionThreeValue !== "YES" && questionThreeValue !== "NO"){
+//       questionThree("Please be serious, Do you need special characters?");
+//       console.log("in here?")
+//     } else if (questionThreeValue === 'NO'){
+//        console.log("nope")
+//     return wordGenerator(parseInt(questionTwoValue),questionThreeValue);
 
-}
+//     } else{
+//       console.log("yup")
+//       return wordGenerator(parseInt(questionTwoValue))
+//     }
+//   }
+// }
+// wordGenerator = function(a,b){
+// // var new_word =[];
+// var newer_word = [];
+// var randomChar = Math.round(Math.random() *128);
+// if(b === "YES"){
+
+//   for(var i = 0; i < a;i++){
+//     randomChar = Math.round(Math.random() *128);
+//     while(randomChar < 31 || randomChar > 127){
+//       randomChar = Math.round(Math.random() *128);
+//       console.log("in yes")
+//     }
+//       // new_word.push(questionOne.charCodeAt(i));
+//       newer_word.push(String.fromCharCode(randomChar));
+
+      
+//   }
+// } else {
+
+//   for(var i = 0; i < a;i++){
+//     randomChar = Math.round(Math.random() *128);
+//     while((randomChar < 65 || randomChar > 90)&&(randomChar < 97 || randomChar > 122)){
+//       randomChar = Math.round(Math.random() *128);
+//     }
+//       // new_word.push(questionOne.charCodeAt(i));
+//       newer_word.push(String.fromCharCode(randomChar));
+     
+  
+// }
+
+//   }
+//   var char_location = newer_word.join('');
+//   console.log(newer_word);
+//   return char_location;
+  
+
+// }
 
