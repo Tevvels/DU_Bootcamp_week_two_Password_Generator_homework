fe85eb218eececa27ad2c9c90426bec2bc9209dd
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-23 15:14:47 -0700

    added a bunch of comments to the script page for clearity

--- script.js
+++ script.js
@@ -14,298 +14,274 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-
-// recreating this project using the Fisher-Yates Shuffle
-
-// function mixArray(array){
-//   var originalIndex = array.length;
-//   var tempValue;
-//   var newIndex;
-
-//   while (originalIndex !== -15){
-//     newIndex = Math.floor(Math.random() * originalIndex);
-//     originalIndex -= 1;
-
-
-//     tempValue = array[originalIndex];
-//     array[originalIndex] = array[newIndex];
-//     array[newIndex] = tempValue;
-
-//   }
-
-//   return array;
-
-// }
-
-
-// var testArray = ['a','b','c','A','B','C',1,2,3,"!","@","#"];
-// mixArray(testArray);
-
-// console.log(testArray);
-
-
-//works well. :D 
-
-
-
-
-
-
-
-
-
-
-
-// var questionaireResults = [];
-// var uppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var lowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var nums = [0,1,2,3,4,5,6,7,8,9];
-// var specials = ["!","@","#","$","%","^","&","&","*","-","=","+","/","|","~","`",".",",","?","<",">","/","_"]
-// var passwordLength;
-
-// var arrayLoop = function(array){
-//   for (var i = 0; i < array.length; i++) {
-//     questionaireResults.push(array[i]);
-    
-//   }
-
-// }
-
-//// customs////
+// The password generator
 
 function generatePassword( ){
 
-var upperArguments = [65,93];
-var lowerArguments = [96,122];
-var numberArguments = [39,48];
-
+// First question asking the length of the password. The password must be within 8 to 127 characters.
 var passwordLengthQuestion = prompt("Choose a password length it can be 8 to 127 ");
-  
+    
+  // a while loop to force the user to answer this question and with the correct values.
 while(passwordLengthQuestion < 7 || passwordLengthQuestion > 127 || passwordLengthQuestion == NaN){
     passwordLengthQuestion = prompt("please choose between 8 and 127 characters in length")
   }; 
 
+  // returning the result and adding it the the passwordLength variable.
+ passwordLength = passwordLengthQuestion;
 
-passwordLength = passwordLengthQuestion;
-var withUppers = confirm("do you need uppercase letters?");
-var withLowers = confirm("do you need lowercase letters?");
-var withNums = confirm("do you need numbers letters?");
-var withSpecials = confirm("do you need specials letters?");
+// the next set of variables are confirms to ask the what criteria the password needs
 
 
-// console.log(mixArray(questionaireResults));
+var withUppers = confirm("do you need uppercase letters?");  // uppercases
+var withLowers = confirm("do you need lowercase letters?"); // lowercases
+var withNums = confirm("do you need numbers letters?"); // numbers
+var withSpecials = confirm("do you need specials letters?"); //special characters or symbols
+
+
+// we then return all 5 of those results into another array to use in the wordGenerator function 
  return wordGenerator(passwordLength,withUppers,withLowers,withNums,withSpecials);
 
 }
 
-// numbers 48 - 57 
-// uppers 65 -88
-// lowers 97-122
-// specials 33 47
+// numbers numbersArguments[0] - numbersArguments[1] 
+// uppers uppersArguments[0] -uppersArguments[1]
+// lowers lowersArguments[0]-lowersArguments[1]
+// specials specialCharsArguments[0] specialCharsArguments[1]
 
 
 
-
+// this function collects the previously gathered information and returns a password
 wordGenerator = function(a,b,c,d,e){
-// var new_word =[];
-var newer_word = [];
-var randomChar = Math.round(Math.random() *128);
-var questionaireResults = [b,c,d,e];
 
-for(i = 0; i < questionaireResults.length;i++){
+  // Set of variable arrays containing the min and max values.
+// To be able to create of numbers to find a specifics using the charcodes.
+
+var uppersArguments = [65,93];  //uppercase letters
+var lowersArguments = [97,122]; //lowercase letters
+var numbersArguments = [48,57]; // numbers ranging from 0 through 9
+var specialCharsArguments = [33,47]; //special characters or symbols
+
+var randomWord = []; // the array that will hold all the of the new characters
+var randomChar = Math.floor(Math.random() *128); // the math.Random function
+var criteriaResults = [b,c,d,e]; // the results of the criteria questions
+
+
+// a for loop that will run through the criteriaResults and return a specific result
+for(i = 0; i < criteriaResults.length;i++){
+
+  // the variables holding the argument values for the while loop
 
   var uppersMin,uppersMax,lowersMin,lowersMax,numsMin,numsMax,specialsMin,specialsMax;
 
 
+    // the if loop that runs through 15 different possible out comes  
+          //uppers                //lowers                              //numbers                 //specials
+     if (criteriaResults[0] && criteriaResults[1] === false && criteriaResults[2] === false && criteriaResults[3] === false){
+
+      //just uppers
+
+      
+      uppersMin = lowersMin = numsMin = specialsMin = uppersArguments[0];
+      uppersMax = lowersMax = numsMax = specialsMax = uppersArguments[1];
+      
+
+}    else if (criteriaResults[0] === false && criteriaResults[1]  && criteriaResults[2] === false && criteriaResults[3] === false){
+
+      //just lowers
   
-     if (questionaireResults[0] && questionaireResults[1] === false && questionaireResults[2] === false && questionaireResults[3] === false){
 
-      uppersMin = 65,lowersMin = 65,numsMin = 65 ,specialsMin = 65;
-      uppersMax = 88, lowersMax = 88, numsMax = 88, specialsMax = 88;
-    console.log(uppersMin,uppersMax,lowersMin,lowersMax,numsMin,numsMax,specialsMin,specialsMax);
-
-}    else if (questionaireResults[0] === false && questionaireResults[1]  && questionaireResults[2] === false && questionaireResults[3] === false){
-     
-
-       uppersMin = 97 ,lowersMin = 97,numsMin = 97,specialsMin = 97;
-       uppersMax = 122,lowersMax = 122,numsMax = 122,specialsMax = 122;
-
-      console.log("just lowers");
+       uppersMin = lowersMin = numsMin = specialsMin = lowersArguments[0];
+       uppersMax = lowersMax = numsMax = specialsMax = lowersArguments[1];
 
 }
-    else if (questionaireResults[0] === false && questionaireResults[1] === false && questionaireResults[2] && questionaireResults[3] === false){
+    else if (criteriaResults[0] === false && criteriaResults[1] === false && criteriaResults[2] && criteriaResults[3] === false){
  
+      //just numbers
 
-           uppersMin = 48,lowersMin = 48,numsMin = 48,specialsMin = 48;
-       uppersMax = 57,lowersMax = 57,numsMax = 57,specialsMax = 57;
 
-      console.log("just numbers");
+      uppersMin = lowersMin = numsMin = specialsMin = numbersArguments[0];
+      uppersMax = lowersMax = numsMax = specialsMax = numbersArguments[1];
 
 }    
-    else if (questionaireResults[0] === false && questionaireResults[1] === false && questionaireResults[2] === false && questionaireResults[3]){
- 
+    else if (criteriaResults[0] === false && criteriaResults[1] === false && criteriaResults[2] === false && criteriaResults[3]){
 
-           uppersMin = 31,lowersMin = 31,numsMin = 31,specialsMin = 31;
-       uppersMax = 47,lowersMax = 47,numsMax = 47,specialsMax = 47;
+      //just specials
 
-      console.log("just specials");
+
+      uppersMin = lowersMin = numsMin = specialsMin = specialCharsArguments[0];
+      uppersMax = lowersMax = numsMax = specialsMax = specialCharsArguments[1];
+
     
-    }  else if (questionaireResults[0] && questionaireResults[1] && questionaireResults[2] === false && questionaireResults[3] === false){
+    }  else if (criteriaResults[0] && criteriaResults[1] && criteriaResults[2] === false && criteriaResults[3] === false){
  
-      uppersMin = 65
+      //just letters
+
+
+      uppersMin = uppersArguments[0]
             
-      lowersMin = 97,numsMin = 97,specialsMin = 97;
+      lowersMin = numsMin = specialsMin = lowersArguments[0];
       
-      uppersMax = 88
+      uppersMax = uppersArguments[1]
       
-      lowersMax = 122,numsMax = 122,specialsMax = 122;
+      lowersMax = numsMax = specialsMax = lowersArguments[1];
 
-      console.log("just letters");
       
-      }else if (questionaireResults[0] && questionaireResults[1] === false && questionaireResults[2]  && questionaireResults[3] === false){
- 
+      }else if (criteriaResults[0] && criteriaResults[1] === false && criteriaResults[2]  && criteriaResults[3] === false){
 
-            uppersMin = 65
+      //uppers and numbers
+        
+
+            uppersMin = uppersArguments[0];
             
-            lowersMin = 48,numsMin = 48,specialsMin = 48;
+            lowersMin = numsMin = specialsMin = numbersArguments[0];
             
-            uppersMax = 88
+            uppersMax = uppersArguments[1];
             
-            lowersMax = 57,numsMax = 57,specialsMax = 57;
+            lowersMax = numsMax = specialsMax = numbersArguments[1];
 
-      console.log("uppers and numbers");
       
-      }else if (questionaireResults[0] && questionaireResults[1] === false && questionaireResults[2] === false && questionaireResults[3]){
+      }else if (criteriaResults[0] && criteriaResults[1] === false && criteriaResults[2] === false && criteriaResults[3]){
+ 
+      //uppers and symbols
+
+
+      uppersMin = uppersArguments[0];
+      lowersMin = numsMin = specialsMin = specialCharsArguments[0];
+      uppersMax = uppersArguments[1];
+      lowersMax = numsMax = specialsMax = specialCharsArguments[1];
+
+      
+      }else if (criteriaResults[0] === false && criteriaResults[1] && criteriaResults[2] && criteriaResults[3] === false){
  
 
-             uppersMin = 65
-             lowersMin = 33,numsMin = 33,specialsMin = 33;
-       uppersMax = 88
-       lowersMax = 47,numsMax = 47,specialsMax = 47;
+      //lowers and numbers
 
-      console.log("uppers and symbols");
+
+      uppersMin = lowersMin = lowersArguments[0];
+      numsMin = specialsMin = numbersArguments[0];
+      uppersMax = lowersMax = lowersArguments[1];
+      numsMax = specialsMax = numbersArguments[1];
+
       
-      }else if (questionaireResults[0] === false && questionaireResults[1] && questionaireResults[2] && questionaireResults[3] === false){
+      }else if (criteriaResults[0] === false && criteriaResults[1] && criteriaResults[2] === false && criteriaResults[3]){
  
+      //lowers and symbols
 
 
+        uppersMin = lowersMin = lowersArguments[0];
+        numsMin = specialsMin = specialCharsArguments[0];
+        uppersMax = lowersMax = lowersArguments[1];
+        numsMax = specialsMax = specialCharsArguments[1];
 
-      uppersMin = 97,lowersMin = 97,
-      numsMin = 48,specialsMin = 48;
-      uppersMax = 122,lowersMax = 122,
-      numsMax = 57,specialsMax = 57;
-
-      console.log("lowers and numbers");
       
-      }else if (questionaireResults[0] === false && questionaireResults[1] && questionaireResults[2] === false && questionaireResults[3]){
+      }else if (criteriaResults[0] === false && criteriaResults[1] === false && criteriaResults[2] && criteriaResults[3]){
  
-
-
-        uppersMin = 97,lowersMin = 97,
-        numsMin = 33,specialsMin = 33;
-        uppersMax = 122,lowersMax = 122,
-        numsMax = 47,specialsMax = 47;
-
-      console.log("lowers and symbols");
-      
-      }else if (questionaireResults[0] === false && questionaireResults[1] === false && questionaireResults[2] && questionaireResults[3]){
- 
-        uppersMin = 33,
-        lowersMin = 33,
-        numsMin = 48,
-        specialsMin = 33;
+      //nums and symbols
+    
+    
+        uppersMin = specialCharsArguments[0];
+        lowersMin = specialCharsArguments[0];
+        numsMin = numbersArguments[0];
+        specialsMin = specialCharsArguments[0];
         
         
-        uppersMax = 47
-        lowersMax = 47,
-        numsMax = 57,
-        specialsMax = 47;
+        uppersMax = specialCharsArguments[1]
+        lowersMax = specialCharsArguments[1];
+        numsMax = numbersArguments[1];
+        specialsMax = specialCharsArguments[1];
 
 
-      console.log("nums and symbols");
       
-      }else if (questionaireResults[0] && questionaireResults[1]  && questionaireResults[2]  && questionaireResults[3] === false){
+      }else if (criteriaResults[0] && criteriaResults[1]  && criteriaResults[2]  && criteriaResults[3] === false){
  
+      //letters an numbers
 
  
-        uppersMin = 65,
-        lowersMin = 97,
-        numsMin = 48,
-        specialsMin = 48;
+        uppersMin = uppersArguments[0];
+        lowersMin = lowersArguments[0];
+        numsMin = numbersArguments[0];
+        specialsMin = numbersArguments[0];
         
         
-        uppersMax = 88
-        lowersMax = 122,
-        numsMax = 57,
-        specialsMax = 57;
+        uppersMax = uppersArguments[1];
+        lowersMax = lowersArguments[1];
+        numsMax = numbersArguments[1];
+        specialsMax = numbersArguments[1];
 
 
 
-      console.log("letters an numbers");
       
-      }else if (questionaireResults[0] && questionaireResults[1]  && questionaireResults[2] === false && questionaireResults[3] ){
+      }else if (criteriaResults[0] && criteriaResults[1]  && criteriaResults[2] === false && criteriaResults[3] ){
+ 
+      //letters an specials
+
+
+        uppersMin = uppersArguments[0];
+        lowersMin = lowersArguments[0];
+        numsMin = specialCharsArguments[0];
+        specialsMin = specialCharsArguments[0];
+        
+        
+        uppersMax = uppersArguments[1];
+        lowersMax = lowersArguments[1];
+        numsMax = specialCharsArguments[1];
+        specialsMax = specialCharsArguments[1];
+
+
+    }else if (criteriaResults[0] && criteriaResults[1] === false && criteriaResults[2] && criteriaResults[3] ){
+      
+      //uppers, numbers and specials
+ 
+ 
+      uppersMin = uppersArguments[0];
+      lowersMin = uppersArguments[0];
+      numsMin = numbersArguments[0];
+      specialsMin = specialCharsArguments[0];
+      
+      
+      uppersMax = uppersArguments[1];
+      lowersMax = uppersArguments[1];
+      numsMax = numbersArguments[1];
+      specialsMax = specialCharsArguments[1];
+
+
+
+      
+      }else if (criteriaResults[0] === false && criteriaResults[1] && criteriaResults[2] && criteriaResults[3]){
  
 
-        uppersMin = 65,
-        lowersMin = 97,
-        numsMin = 33,
-        specialsMin = 33;
+      //lowers, numbers and specials
+
+        uppersMin = lowersArguments[0];
+        lowersMin = lowersArguments[0];
+        numsMin = numbersArguments[0];
+        specialsMin = specialCharsArguments[0];
         
         
-        uppersMax = 88
-        lowersMax = 122,
-        numsMax = 47,
-        specialsMax = 47;
+        uppersMax = lowersArguments[1];
+        lowersMax = lowersArguments[1];
+        numsMax = numbersArguments[1];
+        specialsMax = specialCharsArguments[1];
 
-      console.log("letters an specials");
 
-    }else if (questionaireResults[0] && questionaireResults[1] === false && questionaireResults[2] && questionaireResults[3] ){
       
-      uppersMin = 65,
-      lowersMin = 65,
-      numsMin = 48,
-      specialsMin = 33;
-      
-      
-      uppersMax = 88
-      lowersMax = 88,
-      numsMax = 57,
-      specialsMax = 47;
-
-
-
-      console.log("uppers, numbers and specials");
-      
-      }else if (questionaireResults[0] === false && questionaireResults[1] && questionaireResults[2] && questionaireResults[3]){
- 
-        uppersMin = 97,
-        lowersMin = 97,
-        numsMin = 48,
-        specialsMin = 33;
-        
-        
-        uppersMax = 122
-        lowersMax = 122,
-        numsMax = 57,
-        specialsMax = 47;
-
-
-      console.log("lowers, numbers and specials");
-      
-      }else{
+      }else if (criteriaResults[0] && criteriaResults[1] && criteriaResults[2] && criteriaResults[3]){
    
-      console.log("all");
+      //all
        
  
-       uppersMin = lowersMin = numsMin = specialsMin = 31;
-       uppersMax = lowersMax = numsMax = specialsMax = 127;
+       uppersMin = lowersMin = numsMin = specialsMin = specialCharsArguments[0];
+       uppersMax = lowersMax = numsMax = specialsMax = lowersArguments[1];
       
 
+    } else {
+  
+    // nothing was selected for criteria
+    
+      alert("we need an input please");
+
+       return " : / ";
+     
     }
 
 
@@ -313,61 +289,29 @@ for(i = 0; i < questionaireResults.length;i++){
 
 
 
+    // this for loop calculates the results and runs a Math.random to get a random character for the password
     for(var i = 0; i < a;i++){
-      randomChar = Math.round(Math.random() *128);
+      // the random number stored into random Char
+      randomChar = Math.floor(Math.random() *128);
+      // the loop                  upper criteria                                 lower criteria                          number criteria                                     special criteria
       while((randomChar < uppersMin || randomChar > uppersMax) && (randomChar < lowersMin || randomChar > lowersMax)&&(randomChar < numsMin || randomChar > numsMax)&&(randomChar < specialsMin || randomChar > specialsMax)){
-        randomChar = Math.round(Math.random() *128);
-        console.log("in yes");
+       
+        // a new number incase the last random number did not meet the criteria 
+        randomChar = Math.floor(Math.random() *128);
+
       }
-        newer_word.push(String.fromCharCode(randomChar));
+      
+      // we then place those random characters results into a new array 
+      randomWord.push(String.fromCharCode(randomChar));
 
     }
 
 
+    // we then join the randomword array into one value and store it into the passwordOutcome variable 
+    var passwordOutcome = randomWord.join('');
 
-    var char_location = newer_word.join('');
-    console.log(newer_word);
-    return char_location;
+    // and finally re return the result to the screen 
+    return passwordOutcome;
     
   } 
 }
-
-
-
-
-
-
-
-
-
-
-
-// if(a && b && c && d && e){
-
-//   for(var i = 0; i < a;i++){
-//     randomChar = Math.round(Math.random() *128);
-//     while(randomChar < 31 || randomChar > 127){
-//       randomChar = Math.round(Math.random() *128);
-//       console.log("in yes");
-//     }
-//       // new_word.push(questionOne.charCodeAt(i));
-//       newer_word.push(String.fromCharCode(randomChar));
-
-      
-//   }
-// } else {
-
-//   for(var i = 0; i < a;i++){
-//     randomChar = Math.round(Math.random() *128);
-//     while((randomChar < 65 || randomChar > 90)&&(randomChar < 97 || randomChar > 122)){
-//           randomChar = Math.round(Math.random() *128);
-//         }
-//           // new_word.push(questionOne.charCodeAt(i));
-//          newer_word.push(String.fromCharCode(randomChar));
-//     }
-//   }
-  // var char_location = newer_word.join('');
-  // console.log(newer_word);
-  // return char_location;
-  
-// }
